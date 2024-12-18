908e4405ee630b99865cc060c40d4bf486b3437e
Oxojo <oxojo9x@gmail.com>
2024-12-18 12:52:24 +0900

    メニューのコンポーネントを作成

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="Menu">
+        <div class="Contents">About Me</div>
+        <div class="Contents">Works</div>
+        <div class="Contents">Articles</div>
+    </div>
+</template>
+
+<style>
+.Menu {
+    display: flex;
+    width: 283px;
+    align-items: center;
+    gap: 40px;
+    flex-shrink: 0;
+}
+.Contents {
+    color: #000;
+    font-family: "Inria Sans";
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 160%;
+}
+</style>
