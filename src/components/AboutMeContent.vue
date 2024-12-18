0e8141b6187d23b3c9c3df0376e9aee588977b8d
Oxojo <oxojo9x@gmail.com>
2024-12-18 16:12:32 +0900

    About Me 関連のコンポーネントを作成

---
+++ src/components/AboutMeContent.vue
@@ -0,0 +1,48 @@
+<script setup>
+    import AboutMeIcon from './AboutMeIcon.vue';
+</script>
+
+<template>
+    <div class="Content">
+        <AboutMeIcon />
+        <div class="sentenses">
+            <div class="title">About Me</div>
+            <div class="detail">Oxojo に関する詳しい情報はこちら.</div>
+        </div>
+    </div>
+</template>
+
+<style>
+.Content {
+    display: flex;
+    width: 300px;
+    flex-direction: column;
+    align-items: center;
+    gap: 24px;
+}
+.sentenses {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
+    align-self: stretch;
+}
+.title {
+    color: #000;
+    text-align: center;
+    font-family: "Inria Sans";
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 160%; 
+}
+.detail {
+    color: #000;
+    text-align: center;
+    font-family: "Inria Sans";
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 160%;
+}
+</style>
