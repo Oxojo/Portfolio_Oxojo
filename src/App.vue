5c3befeb829ba725ff825807b82d64d557b513e1
Oxojo <oxojo9x@gmail.com>
2024-12-18 17:01:19 +0900

    フッターを追加

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import HeaderNavi from './components/HeaderNavi.vue';
 import HomeTitle from './components/HomeTitle.vue';
 import ContentsList from './components/ContentsList.vue';
+import CommonFooter from './components/CommonFooter.vue';
 </script>
 
 <template>
@@ -13,8 +14,18 @@ import ContentsList from './components/ContentsList.vue';
     <HomeTitle />
     <ContentsList />
   </main>
+
+  <footer>
+    <CommonFooter />
+  </footer>
 </template>
 
 <style scoped>
-  
+footer {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  background-color: #D9A90E;
+  width: 100vw; 
+}
 </style>
