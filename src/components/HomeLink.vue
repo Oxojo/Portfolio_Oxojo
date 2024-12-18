4686832e4471b4d6d2d3443eaeaca60f64ff3e9c
Oxojo <oxojo9x@gmail.com>
2024-12-18 12:47:11 +0900

    ヘッダー左のコンポーネントを作成

---
+++ src/components/HomeLink.vue
@@ -0,0 +1,26 @@
+<script setup>
+import IconImage from './IconImage.vue';
+</script>
+
+<template>
+    <div class="Home">
+        <IconImage />
+        <div class="Moji">Oxojo.dev</div>
+    </div>
+</template>
+
+<style>
+.Home {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+}
+.Moji {
+    color: #000;
+    font-family: "Inria Sans";
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 160%; /* 38.4px */
+}
+</style>
