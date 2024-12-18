91667b42768d02884ed43d0d2f9c4bbe54d7b181
Oxojo <oxojo9x@gmail.com>
2024-12-18 16:13:21 +0900

    ContentsLists を作成

---
+++ src/components/ContentsList.vue
@@ -0,0 +1,23 @@
+<script setup>
+import AboutMeContent from './AboutMeContent.vue';
+import WorksContent from './WorksContent.vue';
+import ArticlesContent from './ArticlesContent.vue';
+</script>
+
+<template>
+    <div class="ContentsList">
+        <AboutMeContent />
+        <WorksContent />
+        <ArticlesContent />
+    </div>
+</template>
+
+<style>
+.ContentsList {
+    margin-top: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 64px;
+}
+</style>
