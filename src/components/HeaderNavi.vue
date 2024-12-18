893e385c64df75d8094c7f004a55ddee664ad14b
Oxojo <oxojo9x@gmail.com>
2024-12-18 13:21:37 +0900

    ヘッダーの Navigation コンポーネントを作成

---
+++ src/components/HeaderNavi.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HomeLink from './HomeLink.vue';
+import MainMenu from './MainMenu.vue';
+</script>
+
+<template>
+    <div class="Navi">
+        <HomeLink />
+        <MainMenu />
+    </div>
+</template>
+
+<style>
+.Navi {
+    display: flex;
+    width: auto;
+    padding: 10px 40px;
+    justify-content: space-between;
+    align-items: center;
+}
+</style>
