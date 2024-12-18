f2fa1766442c957135fff3416f34eb16d09e6dea
Oxojo <oxojo9x@gmail.com>
2024-12-18 12:35:04 +0900

    アイコンのコンポーネントを作成

---
+++ src/components/IconImage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="icon">
+        <img src="/Oxojo_icon.png">
+    </div>
+</template>
+
+<style>
+img {
+    display: flex;
+    width: 32px;
+    height: 32px;
+    justify-content: center;
+    align-items: center;
+}
+</style>
