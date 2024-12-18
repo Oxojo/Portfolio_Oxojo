3792bd4262ecaf3983a759f76a95467afa26ef49
Oxojo <oxojo9x@gmail.com>
2024-12-18 17:01:14 +0900

    フッターのコンポーネントを追加

---
+++ src/components/CommonFooter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="Footer">
+        (C) 2024 Oxojo
+    </div>
+</template>
+
+<style>
+.Footer {
+    margin: 0;
+    padding: 10px 40px;
+    justify-content: center;
+    align-items: center;
+    font-family: "Inria Sans";
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 160%;
+    color: #000;
+    text-align: center;
+}
+</style>
