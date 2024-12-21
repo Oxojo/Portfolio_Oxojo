00ef6835e1a7445bed6a1241dc41bca3d7aca725
Oxojo <oxojo9x@gmail.com>
2024-12-21 14:51:39 +0900

    Article Card の各コンポーネントを作成

---
+++ src/components/ArticleCardDate.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="Date">
+        <img class="Date_icon" src="../../15335_line.png" alt="">
+        <div class="Date_text">yyyy-mm-dd</div>
+    </div>
+</template>
+
+<style>
+.Date {
+    display: flex;
+    padding: 0 10px;
+    align-items: center;
+    gap: 5px;
+    align-self: stretch;
+}
+.Date_icon {
+    width: 20px;
+    height: 15px;
+}
+.Date_text {
+    color: #000;
+    font-family: "Inria Sans";
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 160%;
+}
+</style>
---
+++ src/components/ArticleCardDetail.vue
@@ -0,0 +1,36 @@
+<script setup>
+import ArticleCardDate from './ArticleCardDate.vue';
+import ArticleCardSite from './ArticleCardSite.vue';
+</script>
+
+<template>
+    <div class="Detail">
+        <div class="Title_text">Title</div>
+        <ArticleCardDate />
+        <ArticleCardSite />
+    </div>
+</template>
+
+<style>
+.Detail {
+    display: flex;
+    width: 300px;
+    height: 80px;
+    padding: 10px 20px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+
+    border-radius: 0 0 6px 6px;
+    border-top: 1px solid #FFD130;
+    background: white;
+}
+.Title_text {
+    color: #000;
+    font-family: "Inria Sans";
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 160%;
+}
+</style>
