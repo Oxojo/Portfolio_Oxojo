5bb5041ec3fad68b1792b63f1cb14cd4694eb7c2
Oxojo <oxojo9x@gmail.com>
2024-12-21 17:54:43 +0900

    とりあえず trap.jp から情報を取得するための諸々をコメントに記載

--- src/ArticlesPage.vue
+++ src/ArticlesPage.vue
@@ -1,4 +1,9 @@
 <script>
+// trap.jp の api は https://blog-admin.trap.jp/ghost/api/admin/posts/?filter=authors.name:oxojo%2Bstatus:published から取れる
+// image_url は feature_image から
+// title は title から
+// date は published_at から (すこし工夫が必要？)
+// link は trap.jp
 import ArticleCard from './components/ArticleCard.vue';
 
 export default {
@@ -19,3 +24,4 @@ export default {
 <template>
     <ArticleCard :image_url="image_url" :title="title" :date="date" :link="link" />
 </template>
+
