c18d2bbb35dae9e3a0f9464e059b38cea2166c73
Oxojo <oxojo9x@gmail.com>
2024-12-23 21:27:28 +0900

    ArticlesCard を複数表示できるようにした

--- src/ArticlesPage.vue
+++ src/ArticlesPage.vue
@@ -12,16 +12,47 @@ export default {
     },
     data() {
         return {
-            image_url: "../Oxojo_icon.png",
-            title: "Title",
-            date: "yyyy-mm-dd",
-            link: "example.com"
+            items: [
+                {imate_url: "../Oxojo_icon.png", title: "Test1", date: "1111-11-11", link: "example.com"},
+                {imate_url: "../Oxojo_icon.png", title: "Test2", date: "2222-22-22", link: "example.com"},
+                {imate_url: "../Oxojo_icon.png", title: "Test3", date: "3333-33-33", link: "example.com"},
+                {imate_url: "../Oxojo_icon.png", title: "Test4", date: "4444-44-44", link: "example.com"}
+            ]
         }
     }   
 };
 </script>
 
 <template>
-    <ArticleCard :image_url="image_url" :title="title" :date="date" :link="link" />
+    <h1 class="title">Articles</h1>
+    <div class="Cards">
+        <ArticleCard 
+            v-for="(item, index) in items"
+            :key="index"
+            :image_url="item.image_url"
+            :title="item.title"
+            :date="item.date"
+            :link="item.link"
+        />
+    </div>
 </template>
 
+<style>
+.title {
+    color: #D9A90E;
+    font-family: "Inria Sans";
+    font-size: 64px;
+    font-style: normal;
+    text-align: center;
+}
+.Cards {
+    padding: 0 50px;
+    width: auto;
+    display: flex;
+    gap: 80px;
+    align-items: flex-start;
+    align-content: flex-start;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+</style>
