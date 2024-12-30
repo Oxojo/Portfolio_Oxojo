4d52e09283e8d8d3212c90426708f155117ff1e3
Oxojo <oxojo9x@gmail.com>
2024-12-30 17:00:19 +0900

    AboutMeTopContent を作成

---
+++ src/components/AboutMeTopContent.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import SNSButtons from './SNSButtons.vue';
+</script>
+
+<template>
+    <div class="Top">
+        <img src="../../Oxojo_icon.png" alt="">
+        <SNSButtons />
+    </div>
+</template>
