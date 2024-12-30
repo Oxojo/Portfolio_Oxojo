d01fd82ea550c71a9e22d06808ff25d2e1cc6433
Oxojo <oxojo9x@gmail.com>
2024-12-30 16:35:43 +0900

    About Me ページのコンポーネントを作成

---
+++ src/AboutMePage.vue
@@ -0,0 +1,25 @@
+<script>
+    import SNSButtons from './components/SNSButtons.vue';
+
+    export default {
+        components: {
+            SNSButtons
+        }
+    }
+</script>
+
+<template>
+    <h1 class="title">About Me</h1>
+    <SNSButtons />
+</template>
+
+<style>
+.title {
+    color: #D9A90E;
+    font-family: "Inria Sans";
+    font-size: 64px;
+    font-style: normal;
+    text-align: center;
+}
+
+</style>
