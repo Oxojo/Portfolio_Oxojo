3de1fe4013a1029f45477a50728768d70975ff3d
Oxojo <oxojo9x@gmail.com>
2024-12-30 17:23:57 +0900

    About Me ページを整備

--- src/AboutMePage.vue
+++ src/AboutMePage.vue
@@ -1,25 +1,87 @@
 <script>
-    import SNSButtons from './components/SNSButtons.vue';
+    import AboutMeTopContent from './components/AboutMeTopContent.vue';
+    
 
     export default {
         components: {
-            SNSButtons
+            AboutMeTopContent
         }
     }
 </script>
 
 <template>
-    <h1 class="title">About Me</h1>
-    <SNSButtons />
+    <div class="Contents">
+        <h1 class="title">About Me</h1>
+        <AboutMeTopContent />
+        <div class="Detail">
+            <div class="ContentTitle">
+                Status
+            </div>
+            <div class="ContentDetail">
+                Science Tokyo B1 <br>
+                Belong to: traP, Tech-nation Records
+            </div>
+        </div>
+        <div class="Detail">
+            <div class="ContentTitle">
+                Interest
+            </div>
+            <div class="ContentDetail">
+                [ Competitive Programming ] <br>
+                AtCoder Cyan <br>
+                Codeforces Specialist (Highest : Expert) <br>
+                [ DTM ] <br>
+                Drum & Bass, House, Trance <br>
+                Acoustic (Piano) <br>
+                [ Web ] <br>
+                Frontend - HTML, CSS, JavaScript (Framework : Vue.js) <br>
+                Backend - Go <br>
+                [ Graphics ] <br>
+                Design (Figma) <br>
+                Pixel Art <br>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style>
+.Contents {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 50px;
+}
 .title {
     color: #D9A90E;
     font-family: "Inria Sans";
     font-size: 64px;
     font-style: normal;
     text-align: center;
+    font-weight: 700;
 }
-
+.ContentTitle {
+    color: #D9A90E;
+    text-align: center;
+    font-family: 'Inria Sans';
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 160%;
+}
+.ContentDetail {
+    color: #000;
+    text-align: center;
+    font-family: 'Inria Sans';
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 160%;
+}
+.Detail {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+}
 </style>
--- src/components/AboutMeTopContent.vue
+++ src/components/AboutMeTopContent.vue
@@ -4,7 +4,32 @@
 
 <template>
     <div class="Top">
-        <img src="../../Oxojo_icon.png" alt="">
+        <img class="AboutmeTopIcon" src="../../Oxojo_icon.png" alt="">
         <SNSButtons />
     </div>
 </template>
+
+<style>
+.AboutmeTopIcon {
+    display: flex;
+    width: 205px;
+    height: 205px;
+    justify-content: center;
+    align-items: center;
+}
+.Top {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 30px;
+}
+@media screen and (max-width: 500px) {
+    .Top {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 30px;
+    }
+}
+</style>
