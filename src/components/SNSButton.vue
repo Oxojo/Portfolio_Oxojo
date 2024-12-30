bbb845b5186032f4ec4f4364be268f8c08c5cb58
Oxojo <oxojo9x@gmail.com>
2024-12-30 16:35:18 +0900

    各種 SNS ボタンを実装

---
+++ src/components/SNSButton.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+    props: {
+        Logo_url: String,
+        Text: String
+    }
+}
+</script>
+
+<template>
+    <div class="Button">
+        <div class="details">
+            <img :src="Logo_url" alt="">
+            <div class="text">{{ Text }}</div>
+        </div>
+    </div>
+</template>
+
+<style>
+.details {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+}
+.Button {
+    display: flex;
+    padding: 10px 20px;
+    flex-direction: column;
+    justify-content: center;
+    gap: 10px;
+    align-self: stretch;
+    align-items: flex-start;
+
+    border-radius: 10px;
+    border: 1px solid #D9A90E;
+    background: white;
+    width: 210px;
+    height: 35px;   
+}
+.text {
+    color: #000;
+    font-family: 'Inria Sans';
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 160%;
+}
+</style>
---
+++ src/components/SNSButtons.vue
@@ -0,0 +1,37 @@
+<script>
+import SNSButton from './SNSButton.vue';
+
+export default {
+    components: {
+        SNSButton
+    },
+    data() {
+        return {
+            items: [
+                { url: "./X_Black.png", text: "X : @Oxojo_" },
+                { url: "./GitHub_Black.png", text: "GitHub : @Oxojo" }
+            ]
+        }
+    }
+}
+</script>
+<template>
+    <div class="Buttons">
+        <SNSButton
+            v-for="(item, index) in items"
+            :key="index"
+            :Logo_url="item.url"
+            :Text="item.text"
+         />
+    </div>
+</template>
+
+<style>
+.Buttons {
+    display: flex;
+    width: 250px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 20px;
+}
+</style>
