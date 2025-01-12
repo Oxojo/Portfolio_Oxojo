da9c2257546641cbc7b4bdc5deb7601553a1f373
Oxojo <oxojo9x@gmail.com>
2025-01-12 13:09:24 +0900

    Works ページのコンポーネントを作成

---
+++ src/WorksPage.vue
@@ -0,0 +1,50 @@
+<script>
+import WorksCard from './components/WorksCard.vue';
+
+export default {
+    components: {
+        WorksCard
+    },
+    data() {
+        return {
+            items: [
+                {image_url: "../Oxojo.dev_ogp.png", title: "Test1", url: "/works"}
+            ]
+        }
+    }   
+};
+</script>
+
+<template>
+    <h1 class="title">Works</h1>
+    <div class="Cards">
+        <WorksCard 
+            v-for="(item, index) in items"
+            :key="index"
+            :image_url="item.image_url"
+            :title="item.title"
+            :url="item.url"
+        />
+    </div>
+</template>
+
+<style>
+.title {
+    color: #D9A90E;
+    font-family: "Inria Sans";
+    font-size: 64px;
+    font-style: normal;
+    text-align: center;
+}
+.Cards {
+    padding: 0 50px;
+    width: auto;
+    display: flex;
+    gap: 80px;
+    align-items: flex-start;
+    align-content: flex-start;
+    flex-wrap: wrap;
+    justify-content: center;
+    border: none;
+}
+</style>
